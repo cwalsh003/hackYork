9c43a2137469fba4d09c92d2a59cc04accebf318
EC2 Default User <ec2-user@ip-172-31-51-27.ec2.internal>
2016-10-22 21:17:04 +0000

    this

--- bossPlaya.py
+++ bossPlaya.py
@@ -15,14 +15,6 @@ def finalRating(lat,lon):
 
     br = browser = mechanicalsoup.Browser()
 
-    # Broser options
-    br.set_handle_equiv( True )
-    #br.set_handle_gzip( True )
-    br.set_handle_redirect( True )
-    br.set_handle_referer( True )
-    br.set_handle_robots( False )
-
-    br.set_handle_refresh( mechanize._http.HTTPRefreshProcessor(), max_time = 1 )
     br.addheaders = [ ( 'User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1' ) ]
 
     lat1 = str(round(float(lat),2))
@@ -31,7 +23,7 @@ def finalRating(lat,lon):
     print(lon1 + '\n')
     url = ("http://api.spotcrime.com/crimes.json?lat=" + lat1 + "&lon=" + lon1 + "&radius=0.2&key=.")
     print(url)
-    br.open(url)
+    br.get(url)
     thepage = browser.get(url)
     print(thepage)
     soup = BeautifulSoup(thepage, "html.parser")

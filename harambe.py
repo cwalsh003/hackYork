b5237e1214fc4bdf78fd9fa554f76bcb340e129a
Colin <c.walsh8600@gmail.com>
2016-10-22 13:36:22 -0400

    prin

--- harambe.py
+++ harambe.py
@@ -4,19 +4,15 @@ app = Flask(__name__)
 
 @app.route('/rating')
 
-def callDevice(deviceID):
+def callDevice():
     location = request.args
-
-    device = devices.get(deviceID)
-
+    print(request.args)
     payload = {
         "geolocation": {
             "lat" : location['lat'],
             "lon" : location['lon'],
             "time": location['time']
         }
-
     }
-
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 8000, debug = True)

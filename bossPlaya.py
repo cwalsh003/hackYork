f57956967c56ef0110a7a62577904c93b17eb0c0
Colin <c.walsh8600@gmail.com>
2016-10-22 09:26:48 -0400

    redis

--- bossPlaya.py
+++ bossPlaya.py
@@ -1,18 +1,56 @@
 import requests
-import json
+import jsonify
 import pprint
+import redis
 # from collections import OrderedDict
 myDict = dict()
-
-r = requests.get('http://api.spotcrime.com/crimes.json?lat=39.962914&lon=-76.75632000000002&radius=0.2&key=.')
+myDict2 = dict()
+ctr = 0
+r = requests.get("http://api.spotcrime.com/crimes.json?lat=39.962914&lon=-76.75632000000002&radius=0.2&key=.")
 myDict = r.json()
+myDict2[1] = {}
+myDict2[1]["rating"] = 1
 
-# print(r.headers['content-type'])
-# print(json.loads(r.text))
-# print(json.dumps(r.text))
+rdb = redis.StrictRedis(host='localhost', port=6379, db=0, password='foobared')
 
 
-for crimes in myDict['crimes']:
-    print(crimes)
-    # for crimes in myDict['crimes'][keys]:
-    #     print(crimes + '\n')
+for crimes in myDict["crimes"]:
+    # print(crimes.keys())
+    ctr = ctr + 1
+    myDict2[ctr] = {}
+    myDict2[ctr]["rating"] = 1
+
+    for majorKey in crimes.keys():
+        if (majorKey == "type" or majorKey == "lat" or majorKey == "lon" or majorKey == "date"):
+            if(majorKey == "lat"):
+                myDict2[ctr]["lat"] = crimes[majorKey]
+            if(majorKey == "lon"):
+                myDict2[ctr]["lon"] = crimes[majorKey]
+            if(majorKey == "date"):
+                myDict2[ctr]["date"] = crimes[majorKey]
+
+            else:
+                if(crimes[majorKey] == "Arrest"):
+                    myDict2[ctr]["rating"] = myDict2[ctr]["rating"] + 5 % 100
+                elif(crimes[majorKey] == "Arson"):
+                    myDict2[ctr]["rating"] = myDict2[ctr]["rating"] + 5 % 100
+                elif(crimes[majorKey] == "Assault"):
+                    myDict2[ctr]["rating"] = myDict2[ctr]["rating"] + 5 % 100
+                elif(crimes[majorKey] == "Burglary"):
+                     myDict2[ctr]["rating"] = myDict2[ctr]["rating"] + 5 % 100
+                elif(crimes[majorKey] == "Robbery"):
+                    myDict2[ctr]["rating"] = myDict2[ctr]["rating"] + 5 % 100
+                elif(crimes[majorKey] == "Shooting"):
+                    myDict2[ctr]["rating"] = myDict2[ctr]["rating"] + 5 % 100
+                elif(crimes[majorKey] == "Theft"):
+                    myDict2[ctr]["rating"] = myDict2[ctr]["rating"] + 5 % 100
+                elif(crimes[majorKey] == "Vandalism"):
+                    myDict2[ctr]["rating"] = myDict2[ctr]["rating"] + 5 % 100
+                elif(crimes[majorKey] == "Other"):
+                    myDict2[ctr]["rating"] = myDict2[ctr]["rating"] + 5 % 100
+        rdb.append(ctr, myDict2)
+
+value = r.get('foo')
+print(value)
+
+            # print(majorKey)

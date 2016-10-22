3f2dd89715f9bcc6ae47e9a7d7cf2ceb8051515d
Colin <c.walsh8600@gmail.com>
2016-10-22 16:50:14 -0400

    this

--- bossPlaya.py
+++ bossPlaya.py
@@ -2,6 +2,7 @@ import requests
 import json
 import pprint
 import redis
+import mechanicalsoup
 from flask import Flask, request, jsonify
 from bs4 import BeautifulSoup
 
@@ -12,13 +13,26 @@ ctr = 0
 #pass location[lat],location[lon] through function
 def finalRating(lat,lon):
 
+    br = browser = mechanicalsoup.Browser()
+
+    # Broser options
+    br.set_handle_equiv( True )
+    #br.set_handle_gzip( True )
+    br.set_handle_redirect( True )
+    br.set_handle_referer( True )
+    br.set_handle_robots( False )
+
+    br.set_handle_refresh( mechanize._http.HTTPRefreshProcessor(), max_time = 1 )
+    br.addheaders = [ ( 'User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1' ) ]
+
     lat1 = str(round(float(lat),2))
     lon1 = str(round(float(lon),2))
     print(lat1 + '\n')
     print(lon1 + '\n')
     url = ("http://api.spotcrime.com/crimes.json?lat=" + lat1 + "&lon=" + lon1 + "&radius=0.2&key=.")
     print(url)
-    thepage = requests.get(url)
+    br.open(url)
+    thepage = browser.get(url)
     print(thepage)
     soup = BeautifulSoup(thepage, "html.parser")
     for crimes in soup.findAll('pre'):

6bc612be47fee0ede2c07bdf106a9af76ef6ae34
Colin <c.walsh8600@gmail.com>
2016-10-22 13:53:46 -0400

    return again

--- harambe.py
+++ harambe.py
@@ -14,6 +14,6 @@ def callDevice():
             "time": location['time']
         }
     }
-    return jsonify('rating': 4)
+    return jsonify(rating = 4)
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 8000, debug = True)

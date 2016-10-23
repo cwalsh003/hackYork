da179a7c460062ac4fabfa2dc4a90771fe87788c
Colin <c.walsh8600@gmail.com>
2016-10-22 22:18:18 -0400

    I quit

--- bossPlaya.py
+++ bossPlaya.py
@@ -1,27 +1,27 @@
-
-
 import requests
 import jsonify
 import json
 import pprint
 import redis
 from flask import Flask, request
+
 # from collections import OrderedDict
 myDict = dict()
 myDict2 = dict()
 ctr = 0
 
-#pass location[lat],location[lon] through function
-def finalRating(lat,lon):
-    lat = str(round(lat,2))
-    lon = str(round(lon,2))
+
+# pass location[lat],location[lon] through function
+def finalRating(lat, lon):
+    lat = str(round(lat, 2))
+    lon = str(round(lon, 2))
 
     url = ("http://api.spotcrime.com/crimes.json?lat=" + lat + "&lon=" + lon + "&radius=0.2&key=.")
 
     r = requests.get(url)
 
     myDict = r.json()
-
+    rating = 0
     latSum = 0
     lonSum = 0
 
@@ -30,19 +30,20 @@ def finalRating(lat,lon):
 
     rdb = redis.StrictRedis(host='54.174.126.53', port=6379, db=0)
 
-
     for crimes in myDict["crimes"]:
         for majorKey in crimes.keys():
             majorKeys.append(majorKey)
-            print(crimes )
-            print( majorKey)
-            if ( majorKey == "lat" or majorKey == "lon"):
-                if(majorKey == "lat"):
-                    latSum += crimes[majorKey]
-                    ctr = ctr + 1
-                if(majorKey == "lon"):
-                    lonSum += crimes[majorKey]
-                    ctr = ctr + 1
+
+
+        print(crimes)
+        print(majorKey)
+        if (majorKey == "lat" or majorKey == "lon"):
+             if (majorKey == "lat"):
+                latSum += crimes[majorKey]
+                ctr = ctr + 1
+            if (majorKey == "lon"):
+                lonSum += crimes[majorKey]
+                ctr = ctr + 1
 
     keyArr.append(latSum / ctr)
     keyArr.append(lonSum / ctr)
@@ -54,30 +55,28 @@ def finalRating(lat,lon):
 
     for keys in myDict2.keys():
         for majorKey in majorKeys:
-            print(keys)
+        print(keys)
 
-            if(crimes[majorKey] == "Arrest"):
-                rating += 5
-            elif(crimes[majorKey] == "Arson"):
-                rating += 8
-            elif(crimes[majorKey] == "Assault"):
-                rating += 5
-            elif(crimes[majorKey] == "Burglary"):
-                rating += 4
-            elif(crimes[majorKey] == "Robbery"):
-                rating += 2
-            elif(crimes[majorKey] == "Shooting"):
-                rating += 7
-            elif(crimes[majorKey] == "Theft"):
-                rating += 4
-            elif(crimes[majorKey] == "Vandalism"):
-                rating += 3
-            elif(crimes[majorKey] == "Other"):
-                rating += 2
+    if (crimes[majorKey] == "Arrest"):
+        rating += 5
+    elif (crimes[majorKey] == "Arson"):
+        rating += 8
+    elif (crimes[majorKey] == "Assault"):
+        rating += 5
+    elif (crimes[majorKey] == "Burglary"):
+        rating += 4
+    elif (crimes[majorKey] == "Robbery"):
+        rating += 2
+    elif (crimes[majorKey] == "Shooting"):
+        rating += 7
+    elif (crimes[majorKey] == "Theft"):
+        rating += 4
+    elif (crimes[majorKey] == "Vandalism"):
+        rating += 3
+    elif (crimes[majorKey] == "Other"):
+        rating += 2
 
-
-
-    myDict2[keyTuple]["rating"]=rating
+    myDict2[keyTuple]["rating"] = rating
 
     rdb.append(keys, myDict2)
 

604314e9d4636fd18df71a9ac0e99eab6213c265
Colin <c.walsh8600@gmail.com>
2016-10-22 22:09:11 -0400

    this is it

--- harambe.py
+++ harambe.py
@@ -1,13 +1,18 @@
+import json
 from flask import request, Flask, jsonify
-import bossPlaya
+
 
 app = Flask(__name__)
 
-@app.route('/rating')
+@app.route('/rating',methods=['GET','POST'])
 
 def callDevice():
     location = request.args
     print(request.args)
+
+    crimedata = request.json
+    print(crimedata)
+    print(json.dumps(crimedata['crimes']))
     payload = {
         "geolocation": {
             "lat" : location['lat'],
@@ -15,6 +20,7 @@ def callDevice():
             "time": location['time']
         }
     }
-    return jsonify(rating = bossPlaya.finalRating(location['lat'],location['lon']))
+    return jsonify(rating=4)
+    #return jsonify(rating = bossPlaya.finalRating(location['lat'],location['lon']))
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 8000, debug = True)
